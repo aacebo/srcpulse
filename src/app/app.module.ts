86bcb1383e62021dafa9937ea1ebc91cfecbaa7d
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2020-08-25 12:00:45 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'srcpulse' }),
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
